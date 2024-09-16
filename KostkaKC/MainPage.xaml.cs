d2c76e32f48b972c17d61978fd7cc6cbf69a3399
Itachii_UwUchiha <konradgd2005@gmail.com>
2024-09-16 14:03:02 +0200

    #Krok 2

--- KostkaKC/MainPage.xaml.cs
+++ KostkaKC/MainPage.xaml.cs
@@ -2,28 +2,40 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
 
         public MainPage()
         {
             InitializeComponent();
         }
-
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
             Random r = new Random();
             int diceRoll = r.Next(1, 7);
             CounterBtn.Text = "Wynik rzutu: " + diceRoll.ToString();
+            int diceRoll = 6;
+            if (K4.IsChecked)
+            {
+                diceRoll = r.Next(1, 5);
+            }
+            if (K6.IsChecked)
+            {
+                diceRoll = r.Next(1, 7);
+            }
+            if (K10.IsChecked)
+            {
+                diceRoll = r.Next(1, 11);
+            }
+            if (K12.IsChecked)
+            {
+
+                diceRoll = r.Next(1, 13);
+            }
+
+            RollLabel.Text = "Wynik rzutu: " + diceRoll.ToString();
         }
+
+
     }
+
 
 }
